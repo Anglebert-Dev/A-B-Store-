b61e201c841f00c057d579f82c827eef0e5605bf
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2025-03-27 16:53:57 +0200

    Add initial layout and component files for the e-commerce site

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,17 @@
+@include('Components.head')
+
+<!-- loader -->
+@include('Components.loader')
+
+<!--header  -->
+@include('Components.nav')
+
+
+
+
+
+<!-- footer -->
+@include('Components.footer')
+
+<!-- scripts -->
+@include('Components.scripts')
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Pages.welcome');
+});
+// products and single-products routes
+Route::get('/products', function () {
+    return view('Pages.Products.products');
+});
+Route::get('/single-product', function () {
+    return view('Pages.Products.single-product');
 });
